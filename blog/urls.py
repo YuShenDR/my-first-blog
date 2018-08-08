c83a7ea651e2d9faa6f238814b4292bc4502ad62
YuShenDR <595383100@qq.com>
2018-08-08 14:43:18 +0800

    Changed the HTML-css for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     #url(r'^$', views.post_list, name='post_list'),
 ]
