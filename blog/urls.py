b6eb540d1a23d1280237b2a0e152b0490a41d73c
YuShenDR <595383100@qq.com>
2018-08-08 16:38:16 +0800

    Changed the create/edit HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    path(r'post/(?P<pk>[0-9]+)/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path(r'post/(?P<pk>[0-9]+)/edit/', views.post_edit, name='post_edit'),
     #url(r'^$', views.post_list, name='post_list'),
 ]
